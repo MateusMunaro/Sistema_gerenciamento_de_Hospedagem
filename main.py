87651b6c3c48f35ffea2c69cc128da6749593671
gabrielchboff <gabrielchagasboff@gmail.com>
2024-04-26 16:22:23 -0300

    feat: new mwnu

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-
-
-def menu():
-    print("""
-    ___Bem vindos a pousada___
-    Opccoes:
-    
-    1 - Consultar disponibilidade de um quarto.
-    2 - Consultar reserva ja feita.
-    3 - Realizar uma reserva.
-    4 - Cancelar reserva.
-    5 - Realizar check-in.
-    6 - Realiza check-out.
-    7 - Registrar consumo.
-    8 - Salvar.
-    0 - Sair   
-             """)
-    
-    menu_choice = str(input('selecione sua escolha:'))
-
-
+from menu import Menu
 
 def main():
-    pass
+    menu_list = [
+        "___ Bem vindos a pousada ___",
+        "Opções:",
+        "1 - Consultar disponibilidade de um quarto.",
+        "2 - Consultar reserva ja feita.",
+        "3 - Realizar uma reserva.",
+        "4 - Cancelar reserva.",
+        "5 - Realizar check-in.",
+        "6 - Realiza check-out.",
+        "7 - Registrar consumo.",
+        "8 - Salvar.",
+        "0 - Sair"
+    ]
+
+    while True:
+        main_menu = Menu(menu_list)
+        opt = main_menu.run()
+
+
+if __name__ == "__main__":
+    main()
