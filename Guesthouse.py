ba0c9224339e393b2f5b267df7df219d5a4dc160
Mateus Augusto Munaro de Souza <mateusmunaro40@gmail.com>
2024-04-26 16:12:08 -0300

    commit

--- Guesthouse.py
+++ Guesthouse.py
@@ -10,4 +10,46 @@ class GuestHouse:
         self.__reservations = reservations
         self.__products = products
 
+        @property
+        def name(self):
+            return self.__name
+        
+        @name.setter
+        def name(self, name: str):
+            self.__name = name
+
+        @property
+        def contact(self):
+            return self.__contact
+        
+        @contact.setter
+        def contact(self, contact: int):
+            self.__contact = contact
+
+        @property
+        def rooms(self):
+            return self.__rooms
+        
+        @rooms.setter
+        def rooms(self, rooms: Room):
+            self.__rooms = rooms
+
+        @property
+        def reservations(self):
+            return self.__reservations
+        
+        @reservations.setter
+        def reservations(self, reservations: Reservation):
+            self.__reservations = reservations
+
+        @property
+        def products(self):
+            return self.__products
+        
+        @products.setter
+        def products(self, products: Product):
+            self.__products = products
+
+        
+
         
